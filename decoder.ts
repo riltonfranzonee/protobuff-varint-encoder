75ee09dfd93690e7e4cba1e93b586cc2ffbd2b29
Rilton Franzone <riltonfranzone@gmail.com>
2023-02-26 08:42:59 -0300

    protobuf decoder

--- decoder.ts
+++ decoder.ts
@@ -1,8 +1,8 @@
 const decoder = (bytes: bigint[]): bigint => {
   bytes = bytes.map((byte, index) => {
     byte = byte & 0x7fn; // discard MSB
-    byte = byte << (7n * BigInt(index)) // shift X bits to the left, depending on the position
-    return byte
+    byte = byte << (7n * BigInt(index)); // shift X bits to the left, depending on the position
+    return byte;
   })
 
   // reverse to put it in little-endian
